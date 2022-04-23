c277bb5a4f59247ca4235de21ea0c2aa5b9b339e
Saharok0707 <saharok0707@gmail.com>
2022-04-23 15:50:17 +0300

    q

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,41 @@
+import React, { useEffect } from 'react';
+import { createPortal } from 'react-dom';
+import s from './Modal.module.css';
+import PropTypes from 'prop-types';
+
+const modalRoot = document.querySelector('#modal-root');
+
+export default function Modal({ onToggleMenu, modalImage }) {
+  useEffect(() => {
+    window.addEventListener('keydown', hendleKeyDownEsc);
+    return () => {
+      window.removeEventListener('keydown', hendleKeyDownEsc);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const hendleKeyDownEsc = e => {
+    if (e.code === 'Escape') {
+      onToggleMenu();
+    }
+  };
+
+  const handleBackdropClick = e => {
+    if (e.target === e.currentTarget) {
+      onToggleMenu();
+    }
+  };
+
+  return createPortal(
+    <div className={s.overlay} onClick={handleBackdropClick}>
+      <div className={s.modal}>
+        <img className={s.image} src={modalImage} alt="LargePhoto" />
+      </div>
+    </div>,
+    modalRoot
+  );
+}
+
+Modal.propTypes = {
+  onClickModal: PropTypes.func,
+};
